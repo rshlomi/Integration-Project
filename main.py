94ee582c704c6eb9e253ef8dcb8452a3718a0300
Ron Shlomi <113076331+rshlomi@users.noreply.github.com>
2022-12-11 09:50:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-# Ron Shlomi
-# Artificial Neural Network - Quadratic Function
+# Artificial Neural Network
+__author__ = "Ron Shlomi"
 
 # TODO
-# max_input outside of NN
+# max_input outside of [0,1]
 # Properly format output
-# higher order polynomial
-# plot actual vs predicted across whole 0-1 range
-# user-determined number of layers
+# Plot actual and predicted polynomail for 0-1 range
+# make code more efficient
 
 import numpy as np
 import torch
@@ -15,25 +14,20 @@ import torch.nn.functional as f
 import matplotlib.pyplot as plt
 
 # Initialize variables
-a, b, c = 5, 0, 0  # polynomial coefficients
 training_iterations = 100
-lines_to_print = 10
-number_to_test = 0.5
+print_every = int(input("How often do you want to print the iterations?"))
 max_input = 1
 losses = list()
-hidden_neurons = list()
-hidden_neurons.append(10)
+coefficients = []
 
+# define class and functions
 class Net(nn.Module):
     def __init__(self):
         """Initializing the network"""
         super(Net, self).__init__()
-        input_neurons = 10
-        self.input_layer = nn.Linear(1, input_neurons)  # 1 by 10 matrix
-        #self.hidden_layer = nn.Linear(layer1_neurons, 10)  # 10 by 10 matrix
-        for neuron in hidden_neurons:
-            self.hidden_layer = nn.Linear(input_neurons, neuron)
-            input_neurons = neuron
+        layer1_neurons = 10
+        self.input_layer = nn.Linear(1, layer1_neurons)  # 1 by 10 matrix
+        self.hidden_layer = nn.Linear(layer1_neurons, 10)  # 10 by 10 matrix
         self.output_layer = nn.Linear(10, 1)  # 10 by 1 matrix
 
     def forward(self, vector):
@@ -45,48 +39,41 @@ class Net(nn.Module):
         vector = self.output_layer(vector)
         return vector
 
-
-def synthetic_target(x, a, b, c):
+def synthetic_target(x, coefficients):
     """Provides numbers for output"""
-    return a * x ** 2 + b * x + c
+    degree = len(coefficients)
+    result = 0
+    for i in range(degree):
+        result += coefficients[i] * x ** i
+    return result
 
 def request_input():
     """Asking for user input to the neural network"""
-    global a, b, c, training_iterations, lines_to_print, number_to_test
-    print("Create a quadratic function")
-    while not isinstance(a, float):
-        try:
-            a = float(input("Pick the coefficient of the squared term"))
-        except:
-            print("Please make sure to enter a number!")
-    while not isinstance(b, float):
-        try:
-            b = float(input("Pick the coefficient of the linear term"))
-        except:
-            print("Please make sure to enter a number!")
-    c = "intentional error"
-    while not isinstance(c, float):
-        try:
-            c = float(input("Pick the intercept"))
-        except:
-            print("Please make sure to enter a number!")
+    # Request the degree of the polynomial from the user
+    degree = int(input("Enter the degree of the polynomial: "))
 
-    training_iterations = int(input("How many iterations would you like?"))
-    lines_to_print = int(input("How many lines would you like to see at the "
-                               "beginning and at the end of training?"))
-    number_to_test = max_input / 2  # later, let user decide
+    # Request the coefficients from the user
 
+    for i in range(degree + 1):
+        coefficient =\
+          float(input("Enter the coefficient for the x^" + str(i) + " term: "))
+        coefficients.append(coefficient)
+    training_iterations = 1
+    while training_iterations < 10:
+        training_iterations = int(input("How many iterations would you like? "))
+    print(f"The network will be trained to emulate polynomials in the (0, {max_input}) domain.")
+    number_to_test = 2
+    while number_to_test < 0 or number_to_test > max_input:
+        number_to_test =\
+            float(input("Please enter a number to test between 0 and 1 "))
 
-def verify_input(training_iterations):
-    """Veryfing if user input is large enough"""
-    if training_iterations < 1:
-        training_iterations = 100
-    else:
-        pass
+    # Pretty print the polynomial using string formatting and list comprehension
+    polynomial_string = " + ".join(
+        [str(coefficients[i]) + "x^" + str(i) for i in range(degree + 1) if coefficients[i] != 0])
+    print("The polynomial is:", polynomial_string)
 
-def present_equation(a, b, c):
-    """Prints the equation"""
-    print(f"Your equation is: {a}x^2 + {b}x + {c}")
+    return coefficients, training_iterations, number_to_test
+
 
 def train_network(max_input, iterations=100, lines=20):
     """Training the network"""
@@ -98,13 +85,13 @@ def train_network(max_input, iterations=100, lines=20):
         # Convert to tensor for pytorch usage
         sample = torch.tensor(sample).float()
 
-        actual_output = synthetic_target(sample, a, b, c)
+        actual_output = synthetic_target(sample, coefficients)
         predicted_output = net(sample)
         loss = loss_function(predicted_output, actual_output)
         losses.append(loss.item())
 
         # print first and last iterations
-        if i < lines_to_print or i > training_iterations - lines_to_print:
+        if i % print_every == 0:
             print(str(i), ": Predicted is ", str(predicted_output.item()) +
                   ". Actual value is ", str(
                 actual_output.item()) + ". Loss is " + str(loss.item()),
@@ -113,25 +100,13 @@ def train_network(max_input, iterations=100, lines=20):
         loss.backward()  # Finds the derivative of the loss in all the parameters
         optimizer.step()  # gradient descent
 
-def test_network(number_to_test, net):
-    """Using a test value, compares predicted to actual value"""
-    number_to_test = np.random.rand(1, 1) * max_input  # Draw random number to seed output
-    number_to_test = torch.tensor(number_to_test).float()
-    actual = synthetic_target(number_to_test, a, b, c)
-    predicted = net(number_to_test)
-    actual_float = synthetic_target(number_to_test, a, b, c).item()
-    predicted_float = net(number_to_test).item()
-
-    # print them both, print difference (absolute and percentage)
-    print(f"Actual is {round(synthetic_target(number_to_test, a, b, c).item(), 2)}",
-          f"Predicted is {round(net(number_to_test).item(), 2)}")
-    print(f"Absolute difference is: {round(predicted_float - actual_float, 2)}")
-    percent_diff = abs(predicted_float) / abs(actual_float + predicted_float)
-    print(f"Percent difference is: {round(percent_diff, 2)}" + "%")
 
 def plot_loss(losses):
     """Plots the loss over time"""
     plt.plot(losses)
+    plt.title("Performance improvement")
+    plt.xlabel("Iteration")
+    plt.ylabel('Loss')
     plt.show()
 
 
@@ -145,14 +120,33 @@ loss_function = nn.MSELoss()
 optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
 
 
+def test_network(number_to_test, net):
+    """Using a test value, compares predicted to actual value"""
+    number_to_test = np.array([number_to_test])
+    number_to_test = torch.tensor(number_to_test).float()
+    actual = synthetic_target(number_to_test, coefficients)
+    predicted = net(number_to_test)
+    actual_float = synthetic_target(number_to_test, coefficients).item()
+    predicted_float = net(number_to_test).item()
+
+    txt0 = "Actual is: {actual_num:.2f}"
+    txt1 = "Predicted is: {predicted_num:.2f}"
+    txt2 = "Absolute difference is: {abs_diff:.2f}"
+    txt3 = "Percent difference is: {cent_diff:.2f} %"
+
+    # print them both, print difference (absolute and percentage)
+    print(txt0.format(actual_num=synthetic_target(number_to_test,\
+                                                  coefficients).item()),
+          txt1.format(predicted_num=net(number_to_test).item()))
+    print(txt2.format(abs_diff=(predicted_float - actual_float)))
+    print(txt3.format(cent_diff=(100 * abs(predicted_float - actual_float)\
+                                  / actual_float)))
 
 
 # Now that all functions have been defined, let's run them
-request_input()
-verify_input(training_iterations)
+coefficients, training_iterations, number_to_test = request_input()
 print("Input verified")
-present_equation(a, b, c)
 print("-" * 79)
-train_network(max_input, training_iterations, lines_to_print)
+train_network(max_input, training_iterations, print_every)
 test_network(number_to_test, net)  # compare network output with polynomial value.
 plot_loss(losses)
